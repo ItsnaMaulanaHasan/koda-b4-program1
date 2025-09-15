0496e45a4fc30bcf206eb71e7e4886c28822d36f
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-15 11:03:41 +0700

    feat: create base view for main menu

---
+++ index.js
@@ -0,0 +1,35 @@
+import { input } from "./interfaceInput.js";
+
+const main = async () => {
+  let loop = true;
+  while (loop) {
+    process.stdout.write("\x1Bc");
+    console.log("\nSilahkan pilih menu\n");
+    console.log("1. Lihat Film");
+    console.log("2. Lihat Keranjang");
+    console.log("3. Lihat History");
+    console.log("4. Keluar\n");
+
+    const inputMenu = await input("Masukkan nomor menu: ");
+
+    switch (inputMenu) {
+      case 1:
+        console.log("Lihat Film");
+        break;
+      case 2:
+        console.log("Lihat Keranjang");
+        break;
+      case 3:
+        console.log("Lihat History");
+        break;
+      case 4:
+        console.log("Keluar");
+        break;
+      default:
+        await input("\n ⚠️ Input tidak sesuai yang diharapkan");
+        break;
+    }
+  }
+};
+
+main();
