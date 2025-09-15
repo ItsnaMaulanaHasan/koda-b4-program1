62099683b704965301fc2892104d7541f8b558fc
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-15 11:45:05 +0700

    feat: added a call to the pilihKendaraan function and adjusted the text in the menu options

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import { input } from "./interfaceInput.js";
+import { pilihKendaraan } from "./lib/kendaraan.js";
 
 const main = async () => {
   let loop = true;
   while (loop) {
     process.stdout.write("\x1Bc");
-    console.log("\nSilahkan pilih menu\n");
-    console.log("1. Lihat Film");
-    console.log("2. Lihat Keranjang");
-    console.log("3. Lihat History");
+    console.log("\nSelamat datang di Hasan Sport Rental, silahkan pilih menu\n");
+    console.log("1. Lihat Kendaraan");
+    console.log("2. List Sewa");
+    console.log("3. History Sewa");
     console.log("4. Keluar\n");
 
     const inputMenu = await input("Masukkan nomor menu: ");
 
     switch (inputMenu) {
       case 1:
-        console.log("Lihat Film");
+        await pilihKendaraan();
         break;
       case 2:
-        console.log("Lihat Keranjang");
+        console.log("List Sewa");
         break;
       case 3:
-        console.log("Lihat History");
+        console.log("History Sewa");
         break;
       case 4:
         console.log("Keluar");
